a66056f0f44c6daae974c9c442b37dc850d01a39
Dulina Duljaya <ultrasploitofficial@gmail.com>
2024-10-05 09:12:24 +0530

    Update index.js

--- backend/inc/index.js
+++ backend/inc/index.js
@@ -1,12 +1,12 @@
 var iframe = document.querySelector("iframe");
 
 if ( sessionStorage.getItem("lastHref") == false ) {
-    sessionStorage.setItem("lastHref", iframe.src)
+    sessionStorage.setItem("lastHref","test")
 
 }   else {
     iframe.src = sessionStorage.getItem("lastHref");
 }
 
 iframe.addEventListener("load", function () {
-    sessionStorage.setItem("lastHref", iframe.src)
+    // sessionStorage.setItem("lastHref", iframe.contentWindow.location.href)
 });
